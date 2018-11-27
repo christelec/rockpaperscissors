3729a976cc9c201dc164c3d75a9f08add75443f1
Christele Cherestal <christele.cherestal@yahoo.com>
2018-11-27 23:03:45 +0000

    computerchoice

--- js/script.js
+++ js/script.js
@@ -8,17 +8,62 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+var randomNumber= 0;
+var userChoice="";
+var computerChoice="";
+var winner="";
+
+
+
 $("#shoot").click(function(){
-    var randomNumber= Math.random();
+    randomNumber= Math.random();
+    userChoice= $("#input").val();
+    console.log(randomNumber);
+    
+$("#userChoice").text(userChoice);
+    
     
     if(randomNumber > .66) {
         $("#computerChoice").text("rock");
 }else if(randomNumber > .33) {
         $("#computerChoice").text("paper");
-}else if(randomNumber > .1)
-        $("#computerChoice").text("scissors"); {
+}else {
+        $("#computerChoice").text("scissors");
 }
-    
+
+ var compare = function(choice1,choice2) {
+            if (choice1 === choice2) {
+                return "It's a tie!";
+            }
+            if (choice1 === "rock") {
+                if (choice2 === "scissors") {
+                    // rock wins
+                    return "You win!";
+                } else {
+                    // paper wins
+                    return "You lose! Try again.";
+                }
+            }
+            if (choice1 === "paper") {
+                if (choice2 === "rock") {
+                    // paper wins
+                    return "You win!";
+                } else {
+                    // scissors wins
+                    return "You lose! Try again.";
+                }
+            }
+            if (choice1 === "scissors") {
+                if (choice2 === "rock") {
+                    // rock wins
+                    return "You lose! Try again.";
+                } else {
+                    // scissors wins
+                    return "You win!";
+                }
+            }
+        };
+   
     
     
 });
